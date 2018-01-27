499cb435eb4a6db7355d81b45ce7ad5486299c2d
qinyuan <pnshsh@163.com>
2018-01-27 23:08:27 +0800

    添加log4j2配置

--- src/main/java/com/pnshsh/mybatis/controller/MetadataController.java
+++ src/main/java/com/pnshsh/mybatis/controller/MetadataController.java
@@ -3,6 +3,8 @@ package com.pnshsh.mybatis.controller;
 import com.pnshsh.mybatis.mapper.MetadataMapper;
 import com.pnshsh.mybatis.service.BeanFieldsFactory;
 import com.pnshsh.mybatis.service.SqlBlockFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.util.Map;
 
 @RestController
 public class MetadataController {
+    private static Logger logger = LoggerFactory.getLogger(MetadataController.class);
 
     @Autowired
     private MetadataMapper metadataMapper;
@@ -25,12 +28,14 @@ public class MetadataController {
 
     @RequestMapping("/databases")
     public List<String> queryDatabase() {
+        logger.info("查询数据库");
         List<String> databases = metadataMapper.queryDatabases();
         return databases;
     }
 
     @RequestMapping("/databases/{dbName}/tables")
     public List<String> queryTables(@PathVariable String dbName) {
+        logger.info("查询表列表");
         List<String> tables = metadataMapper.queryTables(dbName);
         return tables;
     }
